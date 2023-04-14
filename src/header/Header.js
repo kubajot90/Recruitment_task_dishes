a974bf59d25fca624740998533111cd9b3f8d74d
kubajot90 <kubajot90@gmail.com>
2023-04-14 12:40:44 +0100

    Add Header

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,9 +3,21 @@ import classes from "./Header.module.css";
 function Header() {
   return (
     <div className={classes.Header}>
-      <h1>
+      <h1 className={classes.title}>
         Please<br></br> choose a dish and <br></br>enjoy your meal.
       </h1>
+      <p className={classes.description}>
+        If you want more information about this form check out<br></br> my
+        repository on github...
+      </p>
+      <a
+        href="https://github.com/kubajot90/Recruitment_task_dishes"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className={classes.button}>VISIT GITHUB REPO</button>
+      </a>
+      <div className={classes.bg_shape}></div>
     </div>
   );
 }
