445070eb06be4480f138f06a26fb8f535e836f11
kubajot90 <kubajot90@gmail.com>
2023-04-17 20:20:17 +0100

    Add conditionally inputs

--- src/form/Form.js
+++ src/form/Form.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { reduxForm, Field } from "redux-form";
 import classes from "./Form.module.css";
 
 const dishes = ["pizza", "soup", "sandwich"];
 
 function Form({ handleSubmit }) {
+  const [selectedValue, setSelectedValue] = useState("");
+  const [rangeLabel, setRangeLabel] = useState(0);
+
   const submitHandler = (formData) => {
     console.log(formData);
   };
@@ -14,6 +18,18 @@ function Form({ handleSubmit }) {
         {dish}
       </option>
     ));
+  };
+
+  const generateRangeLabel = (e) => {
+    const scale = e.target.value;
+
+    setRangeLabel(scale);
+  };
+
+  const selectChangeHandler = (e) => {
+    const selectedDish = e.target.value;
+
+    setSelectedValue(selectedDish);
   };
 
   return (
@@ -36,14 +52,64 @@ function Form({ handleSubmit }) {
           type="select"
           name="type"
           component="select"
-          placeholder="type"
-          selectedvalue="default"
+          onChange={selectChangeHandler}
         >
-          <option value="default" hidden>
+          <option value="" hidden>
             Chose type
           </option>
           {renderOptions(dishes)}
         </Field>
+
+        {selectedValue === "pizza" && (
+          <>
+            <Field
+              type="number"
+              name="no_of_slices"
+              component="input"
+              placeholder="# of slices"
+            />
+            <Field
+              type="number"
+              step="0.1"
+              min="0"
+              max="100"
+              name="diameter"
+              component="input"
+              placeholder="Diameter"
+            />
+          </>
+        )}
+
+        {selectedValue === "soup" && (
+          <>
+            <label htmlFor="scale"> {rangeLabel} </label>
+            <Field
+              type="range"
+              component="input"
+              id="scale"
+              step="1"
+              min="0"
+              max="10"
+              name="spiciness_scale"
+              placeholder="Spiciness scale"
+              onChange={generateRangeLabel}
+            />
+          </>
+        )}
+
+        {selectedValue === "sandwich" && (
+          <Field
+            // hidden={selectValue !== "sandwich" ? true : false}
+            type="number"
+            component="input"
+            value="1"
+            step="1"
+            min="1"
+            name="slices_of_bread"
+            placeholder="Slices of bread"
+          />
+        )}
+
         <button>Submit</button>
       </form>
     </div>
